ccbc39a834f16b5a52cf76b83ddd57a52035bfa2
wowssun <wow.radiant.ssun@gmail.com>
2024-10-15 17:33:08 +0900

    🎨 Update pagination

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -20,6 +20,7 @@ export async function createPost(formData : FormData) {
 export async function updatePost(formData : FormData) {
     // 아이디 string으로 넘어와서 numner로 변경
     const updateId = Number(formData.get('id'));
+    const updatePage = Number(formData.get('page'));
 
         await prisma.post.update({
             where: { id : updateId },
@@ -28,7 +29,7 @@ export async function updatePost(formData : FormData) {
                 content: formData.get('content') as string,
             },
           })
-          redirect(`/board/view/${updateId}`);
+          redirect(`/board/view/${updateId}?page=${updatePage}`);
 }
 
 // 게시물 삭제
@@ -37,7 +38,7 @@ export async function deletePost(formData : FormData) {
      const deleteId = Number(formData.get('id'));
 
      // 삭제 알림창 안되니까 모달로 해야함?
-     // 일단 삭제는 완료
+     // 일단 삭제는 여기
    await prisma.post.delete({
         where: { id : deleteId },
       })
