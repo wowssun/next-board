c372e0f405d310668b988601fdd7ee46dce3a624
wowssun <wow.radiant.ssun@gmail.com>
2024-10-24 17:22:17 +0900

    🎨 작성자 readOnly 추가

--- src/app/board/write/page.tsx
+++ src/app/board/write/page.tsx
@@ -85,7 +85,7 @@ export default function Page() {
               {errors.title && <span className="text-red-500 text-xs ml-2">{errors.title.message}</span>}
               <div>
                 <label htmlFor="authorId" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">작성자</label>
-                <input type="text" id="authorId" {...register('authorId')} 
+                <input type="text" id="authorId" {...register('authorId')} readOnly
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="작성자를 작성하세요" />
               </div>
               <div className="sm:col-span-2">
