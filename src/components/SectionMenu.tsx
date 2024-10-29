e6aec37fa44c7d385bbf5c34da20475adff4cfa4
wowssun <wow.radiant.ssun@gmail.com>
2024-10-29 16:24:45 +0900

    🎨 이미지 폴더 구조 변경

--- src/components/SectionMenu.tsx
+++ src/components/SectionMenu.tsx
@@ -1,6 +1,6 @@
 export default function SectionMenu(props:any) {
     return (
-        <section className="bg-[url('../images/line-note.jpg')] bg-[length:1061px] bg-center dark:bg-gray-900 pt-20 mb-20">
+        <section className="bg-[url('/images/line-note.jpg')] bg-[length:1061px] bg-center dark:bg-gray-900 pt-20 mb-20">
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
             <h1 className="mb-4 text-4xl text-[#071952] font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
                 {props.title}</h1>
